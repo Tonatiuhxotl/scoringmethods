a3444548114dcfc71e6743e06aeab2cee45aed10
Tonatiuhxotl <41914393+Tonatiuhxotl@users.noreply.github.com>
2026-07-14 04:53:22 -0700

    scoring app
    Code for plotting the shiny app was modified

--- scoringmethods251/app.R
+++ scoringmethods251/app.R
@@ -7,6 +7,10 @@
 #    https://shiny.posit.co/
 #
 
+conditions_error<-read.csv('https://github.com/Tonatiuhxotl/251-project/blob/main/conditions_error_251app.csv?raw=true')
+load(url("https://github.com/Tonatiuhxotl/251-project/blob/main/p_matrix2.rda?raw=true"))
+sim251_data<-read.csv('https://github.com/Tonatiuhxotl/251-project/blob/main/sim251_est_values.csv?raw=true')
+
 library(shiny)
 library(dplyr)
 library(ggplot2)
@@ -24,27 +28,33 @@ ui <- fluidPage(
       #                        "Case 2: Latent Treatment Propensity (X)")),
       hr(),
       h5("Measurement Model Parameters"),
-      helpText("Adjust the following conditions to observe the bias due to the scoring strategy and a full latent approach. True value = 0.60."),
-      helpText("Adjust the number of items used to model the latent variable:"),
-      selectInput("n_items", "Number of Items (Indicators):", 
-                  choices = c(3, 6, 12, 18), selected = 3),
-      helpText("Adjust the measurement quality of the items (similar to reliability):"),
+      helpText("Adjust the following conditions to observe the bias due to the scoring strategy"),
+      helpText("Adjust the communality level:"),
       selectInput("communality", "Item Communality:", 
-                  choices = c("High", "Low"), selected = 'High'),
+                  choices = c("High", "Wide", "Low"), selected = 'High'),
+      helpText("Adjust the magnitude of model error (RMSEA) and form (XL or RC):"),
+      selectInput("moderror", "Error:", 
+                  choices = c("e=0.09 (XL)", "e=0.03 (XL)","e=0", "e=0.03 (RC)", "e=0.09 (RC)"), selected = 'e=0'),
       helpText("Adjust the sample size:"),
       selectInput("N", "Sample Size:", 
-                  choices = c(30, 60, 100, 200, 400, 500, 1000), selected = 30)
+                  choices = c(30, 60, 100, 200, 300, 400), selected = 30),
+      helpText("Adjust the model specification:"),
+      selectInput("specification", "Specification:", 
+                  choices = c("Simple", "Correct"), selected = 'Simple')
       
     ),
     
     mainPanel(
-      plotOutput("mcPlot"),
+      plotOutput("mcPlotA"),
+      plotOutput("mcPlotB"),
+      plotOutput("mcPlotC"),
       hr()
       #h4("ML Regression Estimates"),
       #verbatimTextOutput("mcSummary")
     )
   )
 )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -53,145 +63,133 @@ server <- function(input, output) {
     # Fixing seed for stable comparison when parameters are toggled
     set.seed(2026) 
     N <- as.numeric(input$N)
-    communalities <- data.frame(LOW = c(0.2, 0.3, 0.4, 0.2, 0.3, 0.4), 
-                                #WIDE = c(0.2, 0.32, 0.44, 0.56, 0.68, 0.8),
-                                HIGH = c(0.6, 0.7, 0.8, 0.6, 0.7, 0.8))
-    phi <- matrix(c(
-      1.000, 0.6, # Y
-      0.6, 1.000 # X
-    ), ncol = 2, byrow = TRUE)
-    
-    n_items <- as.numeric(input$n_items)
-    lambda <- matrix(0, nrow = (n_items*2), ncol = 2)
-    idx_Y <- 1:n_items
-    num_var <- n_items*2
-    idx_X <- (n_items+1):num_var
-    #lambda2_X <- 1
-    # Set parameters based on communality choice
-    if (input$communality == "High") {
-      lambda2_X<- lambda2_Y <- rep_len(pull(communalities['HIGH']), n_items)
-      lambda[idx_Y, 1] <- sqrt(lambda2_Y)
-      lambda[idx_X, 2] <- sqrt(lambda2_X)
-      Lambda_full <- lambda
-      theta <- diag(num_var) - diag(diag(Lambda_full %*% phi %*% t(Lambda_full)))
-      # Implied Cov matrix
-      sigma0_kappa <- Lambda_full %*% phi %*% t(Lambda_full) + theta
-      sigma0_kappa <- (sigma0_kappa + t(sigma0_kappa))/2 
-      colnames(sigma0_kappa) <- c(paste0('y', 1:n_items), paste0('x', 1:n_items))
-      
-    } else {
-      lambda2_X<- lambda2_Y <- rep_len(pull(communalities['LOW']), n_items)
-      lambda[idx_Y, 1] <- sqrt(lambda2_Y)
-      lambda[idx_X, 2] <- sqrt(lambda2_X)
-      Lambda_full <- lambda
-      theta <- diag(num_var) - diag(diag(Lambda_full %*% phi %*% t(Lambda_full)))
-      # Implied Cov matrix
-      sigma0_kappa <- Lambda_full %*% phi %*% t(Lambda_full) + theta
-      sigma0_kappa <- (sigma0_kappa + t(sigma0_kappa))/2 
-      colnames(sigma0_kappa) <- c(paste0('y', 1:n_items), paste0('x', 1:n_items))
-    }
+    communality <-as.character(input$communality)
+    moderror <- as.character(input$moderror)
+    specification <- as.character(input$specification)
+    num_var<-18
+    n_items<-6
+    idx_eta1<-c(1:6)
+    idx_eta2<-c(7:12)
+    idx_eta3<-c(13:18)
+    k<-conditions_error[which(conditions_error$communality == communality & conditions_error$moderror == moderror), 'Id']
+    sigma0_kappa <- p_matrix[[k]]
     
     # True Causal Structure (SEM)
     dat <- as.data.frame(rmvnorm(N,
                                  mean = rep(0,num_var),
-                                 sigma = sigma0_kappa)) 
-    colnames(dat) <- c(paste0('y', 1:n_items), paste0('x', 1:n_items))
-    
-    model <-  c(paste0("etaY =~ NA*", paste0(paste0("y", 1:n_items), collapse = " + ")),
-                paste0("etaX =~ NA*", paste0(paste0("x", 1:n_items), collapse = " + ")),
-                "etaY  ~~ varetaY*etaY",
-                "etaX  ~~ 1*etaX",
-                "etaY  ~ a*etaX",
-                "varetaY==1-a^2"
-    )
-    
-    model.fit<-sem(model, data =dat )
-    model.params <- parameterEstimates(model.fit, standardized = TRUE)
-    b1 <- model.params[which(model.params$label==c('a')), 'est']
+                                 sigma = )) 
+    colnames(dat) <- paste0('y', 1:num_var)
     
     # Sum Scores
-    vsY <- t(rep(1, n_items)) %*% sigma0_kappa[idx_Y,idx_Y] %*% rep(1, n_items)
-    dat$sumY <- 1/sqrt(vsY[1,1])*rowSums(dat[idx_Y])
-    vsX <- t(rep(1, n_items)) %*% sigma0_kappa[idx_X,idx_X] %*% rep(1, n_items)
-    dat$sumX <- 1/sqrt(vsX[1,1])*rowSums(dat[idx_X])
+    vsEta1 <- t(rep(1, n_items)) %*% sigma0_kappa[idx_eta1,idx_eta1] %*% rep(1, n_items)
+    dat$eta1_sum <- 1/sqrt(vsEta1[1,1])*rowSums(dat[idx_eta1])
+    vsEta2 <- t(rep(1, n_items)) %*% sigma0_kappa[idx_eta2,idx_eta2] %*% rep(1, n_items)
+    dat$eta2_sum <- 1/sqrt(vsEta2[1,1])*rowSums(dat[idx_eta2])
+    vsEta3 <- t(rep(1, n_items)) %*% sigma0_kappa[idx_eta3,idx_eta3] %*% rep(1, n_items)
+    dat$eta3_sum <- 1/sqrt(vsEta3[1,1])*rowSums(dat[idx_eta3])
     
-    dat$sumY_std <- as.numeric(scale(dat$sumY))
-    dat$sumX_std <- as.numeric(scale(dat$sumX))
-    model.sum <- lm(sumY_std ~ sumX_std, data = dat)
+    dat$eta1_sum_std <- as.numeric(scale(dat$eta1_sum))
+    dat$eta2_sum_std <- as.numeric(scale(dat$eta2_sum))
+    dat$eta3_sum_std <- as.numeric(scale(dat$eta3_sum))
     
-    # FS Regression & Bartlett
-    model.fy <-  c(paste0("FY =~ NA*", paste0(paste0("y", 1:n_items), collapse = " + ")),
-                   "FY  ~~ 1*FY"
-    )
-    model.fx <-  c(paste0("FX =~ NA*", paste0(paste0("x", 1:n_items), collapse = " + ")),
-                   "FX  ~~ 1*FX"
-    )
-    model.fy.fit<-cfa(model.fy, data =dat )
-    model.fx.fit<-cfa(model.fx, data =dat )
-    dat[, c("FrY")] <- lavPredict(model.fy.fit, method="regression")
-    dat[, c("FbY")] <- lavPredict(model.fy.fit, method='Bartlett')
-    dat[, c("FrX")] <- lavPredict(model.fx.fit, method="regression")
-    dat[, c("FbX")] <- lavPredict(model.fx.fit, method='Bartlett')
-    #model.rr <- lm(FrY ~ FrX, data = dat)
-    #model.br <- lm(FbY ~ FbX, data = dat) 
-    #model.br.r <- lm(FbY ~ FrX, data = dat) 
+    filter_data <- sim251_data %>% 
+      filter(NSIZE == N &
+               MODERROR == moderror &
+               COMMUNALITY == communality & 
+               SPECIFICATION == specification)
     
-    model.reg <-  c(paste0("etaY =~ NA*", paste0(paste0("y", 1:n_items), collapse = " + ")),
-                    "etaY  ~~ varetaY*etaY",
-                    "etaY  ~ a*FrX",
-                    "varetaY==1-a^2"
-    )
-    model.reg.fit<-sem(model.reg, data =dat )
-    model.reg.params <- parameterEstimates(model.reg.fit, standardized = TRUE)
-    b1.reg <- model.params[which(model.reg.params$label==c('a')), 'est']   
+    a.sem <- filter_data[which(filter_data$PAR=='a' & filter_data$METHOD == 'SEM'), 'est']
+    a.ilv <- filter_data[which(filter_data$PAR=='a' & filter_data$METHOD == 'ILV'), 'est']   
+    a.sum <- filter_data[which(filter_data$PAR=='a' & filter_data$METHOD == 'SS'), 'est'] 
+    a.reg <- filter_data[which(filter_data$PAR=='a' & filter_data$METHOD == 'RS'), 'est'] 
+    a.bar <- filter_data[which(filter_data$PAR=='a' & filter_data$METHOD == 'BS'), 'est']
     
-    model.bar <-  c(paste0("etaY =~ NA*", paste0(paste0("y", 1:n_items), collapse = " + ")),
-                    "etaY  ~~ varetaY*etaY",
-                    "etaY  ~ a*FbX",
-                    "varetaY==1-a^2"
-    )
-    model.bar.fit<-sem(model.bar, data =dat )
-    model.bar.params <- parameterEstimates(model.bar.fit, standardized = TRUE)
-    b1.bar <- model.params[which(model.bar.params$label==c('a')), 'est'] 
+    b.sem <- filter_data[which(filter_data$PAR=='b' & filter_data$METHOD == 'SEM'), 'est']
+    b.ilv <- filter_data[which(filter_data$PAR=='b' & filter_data$METHOD == 'ILV'), 'est']   
+    b.sum <- filter_data[which(filter_data$PAR=='b' & filter_data$METHOD == 'SS'), 'est'] 
+    b.reg <- filter_data[which(filter_data$PAR=='b' & filter_data$METHOD == 'RS'), 'est'] 
+    b.bar <- filter_data[which(filter_data$PAR=='b' & filter_data$METHOD == 'BS'), 'est']
     
-    model.sum <-  c(paste0("etaY =~ NA*", paste0(paste0("y", 1:n_items), collapse = " + ")),
-                    "etaY  ~~ varetaY*etaY",
-                    "etaY  ~ a*sumX",
-                    "varetaY==1-a^2"
-    )
-    model.sum.fit<-sem(model.sum, data =dat )
-    model.sum.params <- parameterEstimates(model.sum.fit, standardized = TRUE)
-    b1.sum <- model.params[which(model.sum.params$label==c('a')), 'est'] 
+    c.sem <- filter_data[which(filter_data$PAR=='c' & filter_data$METHOD == 'SEM'), 'est']
+    c.ilv <- filter_data[which(filter_data$PAR=='c' & filter_data$METHOD == 'ILV'), 'est']   
+    c.sum <- filter_data[which(filter_data$PAR=='c' & filter_data$METHOD == 'SS'), 'est'] 
+    c.reg <- filter_data[which(filter_data$PAR=='c' & filter_data$METHOD == 'RS'), 'est'] 
+    c.bar <- filter_data[which(filter_data$PAR=='c' & filter_data$METHOD == 'BS'), 'est']
     
     # Estimates table
-    df_sem <- data.frame(intercept = 0,  slope = b1, Model = "SEM")
-    df_sum <- data.frame(intercept = 0,  slope = b1.sum, Model = "SS")
-    df_reg <- data.frame(intercept = 0,  slope = b1.reg, Model = "RS")
-    df_bar <- data.frame(intercept = 0,  slope = b1.bar, Model = "BS")
-    #df_br <- data.frame(intercept = 0,  slope = b1, Model = "BR-S")
-    df_true<- data.frame(intercept = 0,  slope = .6, Model = "True DGP")
+    df_sem <- data.frame(a_path = a.sem, b_path = b.sem,  c_path = c.sem, Model = "SEM")
+    df_ilv <- data.frame(a_path = a.ilv, b_path = b.ilv,  c_path = c.ilv, Model = "ILV")
+    df_sum <- data.frame(a_path = a.sum, b_path = b.sum,  c_path = c.sum, Model = "SS")
+    df_reg <- data.frame(a_path = a.reg, b_path = b.reg,  c_path = c.reg, Model = "RS")
+    df_bar <- data.frame(a_path = a.bar, b_path = b.bar,  c_path = c.bar, Model = "BS")
+    df_true<- data.frame(a_path = 0.3, b_path = 0.7,  c_path = 0, Model = "True")
     
-    return(list(lines_df = rbind(df_sem, df_sum, df_reg, df_bar, df_true), raw_data = dat))
+    return(list(lines_df = rbind(df_sem, df_ilv, df_sum, df_reg, df_bar, df_true), raw_data = dat))
   })
   
-  output$mcPlot <- renderPlot({
+  output$mcPlotA <- renderPlot({
     res <- sim_data()
     lines_df <- res$lines_df
     raw_data <- res$raw_data
     
     ggplot() +
       # 1. Plot raw data points to establish scales (using x and the calculated sumY)
-      geom_point(data = raw_data, aes(x = sumX_std, y = sumY_std), alpha = 0.4, color = "grey") +
+      geom_point(data = raw_data, aes(x = eta1_sum_std, y = eta2_sum_std), alpha = 0.4, color = "grey") +
       
       # 2. Draw lines using slope and intercept parameters from your simulation dataframe
-      geom_abline(data = lines_df, aes(intercept = intercept, slope = slope, color = Model, linetype = Model), linewidth = .8, alpha = 0.5) +
+      geom_abline(data = lines_df, aes(intercept = 0, slope = a_path, color = Model, linetype = Model), linewidth = .8, alpha = 0.5) +
       
       # 3. Aesthetics
-      scale_color_manual(values = c("SEM" = "green", "SS" = "pink", "RS" = "#377EB8", "BS" = "#377EB8", "True DGP" = "orange")) +
-      scale_linetype_manual(values = c("SEM" = "solid", "SS" = "solid", "RS" = "dotted", "BS" = "dashed", "True DGP" = "solid")) +
-      labs(title = "Error free estimation by modeling lantent variables",
-           x = "Predictor Variable (X)",
-           y = "Outcome Variable (Y)") +
+      scale_color_manual(values = c("SEM" = "darkgreen", "ILV" = "darkgreen","SS" = "pink", "RS" = "#377EB8", "BS" = "#377EB8", "True" = "orange")) +
+      scale_linetype_manual(values = c("SEM" = "dotted", "ILV" = "dashed", "SS" = "solid", "RS" = "dotted", "BS" = "dashed", "True" = "solid")) +
+      labs(title = "A path (b_21)",
+           x = "Predictor (Eta1)",
+           y = "Mediator (Eta2)") +
+      theme_minimal() +
+      theme(legend.position = "bottom")
+  })
+  
+  
+  output$mcPlotB <- renderPlot({
+    res <- sim_data()
+    lines_df <- res$lines_df
+    raw_data <- res$raw_data
+    
+    ggplot() +
+      # 1. Plot raw data points to establish scales (using x and the calculated sumY)
+      geom_point(data = raw_data, aes(x = eta2_sum_std, y = eta3_sum_std), alpha = 0.4, color = "grey") +
+      
+      # 2. Draw lines using slope and intercept parameters from your simulation dataframe
+      geom_abline(data = lines_df, aes(intercept = 0, slope = b_path, color = Model, linetype = Model), linewidth = .8, alpha = 0.5) +
+      
+      # 3. Aesthetics
+      scale_color_manual(values = c("SEM" = "darkgreen", "ILV" = "darkgreen","SS" = "pink", "RS" = "#377EB8", "BS" = "#377EB8", "True" = "orange")) +
+      scale_linetype_manual(values = c("SEM" = "dotted", "ILV" = "dashed", "SS" = "solid", "RS" = "dotted", "BS" = "dashed", "True" = "solid")) +
+      labs(title = "B path (b_32)",
+           x = "Mediator (Eta2)",
+           y = "Outcome (Eta3)") +
+      theme_minimal() +
+      theme(legend.position = "bottom")
+  })
+  
+  output$mcPlotC <- renderPlot({
+    res <- sim_data()
+    lines_df <- res$lines_df
+    raw_data <- res$raw_data
+    
+    ggplot() +
+      # 1. Plot raw data points to establish scales (using x and the calculated sumY)
+      geom_point(data = raw_data, aes(x = eta1_sum_std, y = eta3_sum_std), alpha = 0.4, color = "grey") +
+      
+      # 2. Draw lines using slope and intercept parameters from your simulation dataframe
+      geom_abline(data = lines_df, aes(intercept = 0, slope = c_path, color = Model, linetype = Model), linewidth = .8, alpha = 0.5) +
+      
+      # 3. Aesthetics
+      scale_color_manual(values = c("SEM" = "darkgreen", "ILV" = "darkgreen","SS" = "pink", "RS" = "#377EB8", "BS" = "#377EB8", "True" = "orange")) +
+      scale_linetype_manual(values = c("SEM" = "dotted", "ILV" = "dashed", "SS" = "solid", "RS" = "dotted", "BS" = "dashed", "True" = "solid")) +
+      labs(title = "C path (b_31)",
+           x = "Predictor (Eta1)",
+           y = "Outcome (Eta3)") +
       theme_minimal() +
       theme(legend.position = "bottom")
   })
